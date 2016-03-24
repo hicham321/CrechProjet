c90da5e04d176fa2952fbe27c1853d8db235721a
Gherbi Hicham <gherbixhicham@gmail.com>
2016-03-24 18:45:28 +0100

    connection to databse

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,49 @@
+import java.sql.*;
+
 
 public class Model {
+	
+	private Connection con ;
+	
+	private Statement stmt;
+	
+	
+	
+	public void connectio ( String path ){
+		
+		
+		
+	    try {
+	        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+	        this.con = DriverManager.getConnection(path, "", "");
+
+	        this.stmt = con.createStatement();
+
+	        // Returns a ResultSet that contains the data produced by the query;
+	        // never null
+	        String query= "insert query here";
+	        ResultSet rs = stmt.executeQuery(query);
+
+
+	        while (rs.next()) {
+	            /*String fName = rs.getString("Field1");
+	           String lName = rs.getString("LastName");
+	            int age = rs.getInt("ID");*/
+	        }
+
+	        stmt.close();
+
+	        con.close();
+	    } catch (SQLException ex) {
+	        System.err.println("SQLException: " + ex.getMessage());
+	    } 
+	      catch (ClassNotFoundException e) {
+	    	  
+		    System.err.println("classnotfoundException: " + e.getMessage());
+
+	    	  
+	    }
+	}
 
 }
