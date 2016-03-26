35e82ee07340eace83e8bffeaedbacd067bee0ab
hicham321 <gherbixhicham@gmail.com>
2016-03-26 15:55:26 +0100

    adding to view

--- src/ViewAjoutEnf.java
+++ src/ViewAjoutEnf.java
@@ -1,9 +1,12 @@
+import java.awt.Panel;
+
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-public class View extends JFrame {
+public class ViewAjoutEnf extends JFrame {
 	
 	private JLabel AjoutEnf = new JLabel("Ajouté Un Enfant");
 	
@@ -13,7 +16,14 @@ public class View extends JFrame {
 	
 	
 	
-	public View (){
+	public ViewAjoutEnf (){
+		
+		
+		Panel p = new Panel();
+		
+		p.add(this.AjoutEnf);
+		p.add(this.Ok);
+		p.add(this.Annuler);
 	
 		
 		
