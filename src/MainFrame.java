4da041b120ac7357977c0bda0aad4c07926e5a37
Gherbi Hicham <gherbixhicham@gmail.com>
2016-05-15 07:38:34 +0100

    adding menus

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,6 +6,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
@@ -14,9 +15,26 @@ public class MainFrame extends JFrame {
 	
 	JButton but = new JButton("sdf");
 	
+	JMenu menuouvrir = new JMenu("Base");
+	
+	JMenuItem ajoutbase = new JMenuItem("Ouvrir");
+	
+	
 	JMenu menuenf = new JMenu("Enfant");
 	
+	JMenuItem ajoutEnf = new JMenuItem("Ajout Enf");
+	
+	JMenuItem listeEnf = new JMenuItem("Liste Enf");
+
+	JMenuItem explore = new JMenuItem("explore");
+	
 	JMenu menuemp = new JMenu("Employ�");
+	
+     JMenuItem ajoutemp = new JMenuItem("Ajout Emp");
+	
+	JMenuItem listeEmp = new JMenuItem("Liste Emp");
+
+	
 	
 	public MainFrame(){
 		
@@ -31,13 +49,19 @@ public class MainFrame extends JFrame {
         
         panelMain.add(menubar);
         menubar.setBounds(0, 0, 400, 30);
-        //
-        //menuenf.setVisible(true);
-        menuenf.add("home");
-        menuenf.add("about");
-        menuenf.add("explore");
         
+        menuouvrir.add(ajoutbase);
+        
+        menuenf.add(ajoutEnf);
+        menuenf.add(listeEnf);
+        menuenf.add(explore);
+        
+        menuemp.add(ajoutemp);
+        menuemp.add(listeEmp);
+        
+        menubar.add(menuouvrir);
         menubar.add(menuenf);
+        menubar.add(menuemp);
        
         panelMain.setBackground(Color.white);
         panelMain.setBounds(0, 0, 400, 400);
@@ -45,5 +69,8 @@ public class MainFrame extends JFrame {
         add(panelMain);
 		
 	}
+	
+		
+	
 
 }
